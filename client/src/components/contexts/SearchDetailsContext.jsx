2d4646af6f6d962f75e17f465fe5450281bf6318
Luke1606 <luisalbertohedzro@gmail.com>
2025-02-22 21:42:25 -0500

    Se renombro y cambio la implementacion del searchContext

---
+++ client/src/components/contexts/SearchDetailsContext.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from 'react'
+import PropTypes from "prop-types"
+
+const SearchDetailsContext = createContext();
+
+const SearchDetailsProvider = ({ children }) => {
+  const [searchTerm, setSearchTerm] = useState("")
+  const [resultOrder, setResultOrder] = useState("")
+  const [resultFilter, setResultFilter] = useState({
+    appearedInLastMonths: false,
+    appearedInLastYear: false,
+    appearedInLastFiveYears: false,
+  })
+
+  return (
+    <SearchDetailsContext.Provider value={{ searchTerm, resultOrder, resultFilter, setSearchTerm, setResultOrder, setResultFilter }}> {children}
+      {children}
+    </SearchDetailsContext.Provider>
+  )
+}
+
+SearchDetailsProvider.propTypes = { children: PropTypes.node.isRequired }
+
+export { SearchDetailsContext, SearchDetailsProvider };
