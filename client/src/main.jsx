058be93a0348cc44a68e02098c2f049dca7c9272
Luke1606 <luisalbertohedzro@gmail.com>
2025-02-22 21:45:01 -0500

    Se actualizo el main teniendo en cuenta los cambios en el searchContext/Provider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { AfterSearchProvider } from './components/contexts/AfterSearchContext.jsx'
+import { SearchDetailsProvider } from './components/contexts/SearchDetailsContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <AfterSearchProvider>
+    <SearchDetailsProvider>
       <App />
-    </AfterSearchProvider>
+    </SearchDetailsProvider>
   </StrictMode>
 );
