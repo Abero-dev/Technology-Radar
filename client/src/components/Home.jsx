91a9409d3fa9a85995da6a81249de8d132951b1d
Luke1606 <luisalbertohedzro@gmail.com>
2025-02-23 17:52:26 -0500

    Se reimplemento el footer como parte del home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,7 +1,50 @@
+import HomeFooter from "./HomeFooter"
 
 const Home = () => {
     return (
-        <div className='home'>Home</div>
+        <>
+            <div className='home'>
+                <h1>Home</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facilis saepe assumenda ad minus blanditiis nihil, nobis reprehenderit non minima incidunt maiores et culpa ea provident excepturi a perferendis eligendi adipisci!</p>
+            </div>
+            
+            <HomeFooter /> 
+        </>
     )
 }
 
