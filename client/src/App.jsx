657c3a69d85e9fd5c0f0096e5337b022f7e40b24
Luke1606 <luisalbertohedzro@gmail.com>
2025-02-22 20:42:07 -0500

    Se cambio la estructura del app.jsx acorde a la version actual de react-router-dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,28 @@
-
-import { Footer } from './components/Footer.jsx';
-import { Home } from './components/Home';
-import { Navbar } from './components/Navbar.jsx';
-import { OtherComponent } from './components/OtherComponent';
+import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from "react-router-dom"
+import { Footer } from './components/Footer.jsx'
+import { Home } from './components/Home'
+import { Navbar } from './components/Navbar.jsx'
+import { OtherComponent } from './components/OtherComponent'
 import RadarComponent from './components/Radar/RadarComponent'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App() {
+const App = () => {
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <>
+                <Route path='/' element={<Home />} />
+                <Route path='/radar' element={<RadarComponent />} />
+                <Route path='/other-option' element={<OtherComponent />} />
+            </>
+        )
+    )
 
-  return (
-    <BrowserRouter>
-      <div className='app'>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home></Home>} />
-          <Route path='/radar' element={<RadarComponent></RadarComponent>} />
-          <Route path='/other-option' element={<OtherComponent></OtherComponent>}></Route>
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  )
+    return (
+        <div className='app'>
+            <Navbar />
+            <RouterProvider router={router} />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+export default App
