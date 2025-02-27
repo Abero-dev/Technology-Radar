ed638f7a064ebfa557b7f03f073b508f8d6a55e9
Luke1606 <luisalbertohedzro@gmail.com>
2025-02-27 16:21:55 -0500

    reguero

---
+++ client/src/components/Radar/RadarRing.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const RadarRings = ({ config }) => {
+    return (
+        <g>
+            {/* Aquí iría la lógica para dibujar los anillos */}
+        </g>
+    );
+};
+
+RadarRings.propTypes = {
+    config: PropTypes.object.isRequired,
+};
+
+export default RadarRings;
