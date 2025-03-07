e8ca51bd5f637bc19adeadf5fa66ee03dd557e74
Abero <abel04.mata@gmail.com>
2025-03-07 16:17:17 -0500

    Se arreglo el problema del drop down menu de Servicios

--- client/src/components/Layout/Navbar.jsx
+++ client/src/components/Layout/Navbar.jsx
@@ -1,34 +1,39 @@
 import { NavLink } from 'react-router-dom';
-import { useState, useRef, useEffect, useCallback } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { ServiceCard } from '../ServiceCard';
 import Loading from '../Loading';
 
 const Navbar = () => {
     const [menuVisible, setMenuVisible] = useState(false);
     const menuRef = useRef(null);
+    const buttonRef = useRef(null);
     const [isLoading, setIsLoading] = useState(false)
 
     useEffect(() => {
         const handleClickOutside = (event) => {
-            if (menuRef.current && !menuRef.current.contains(event.target)) {
+            if (
+                menuRef.current &&
+                !menuRef.current.contains(event.target) &&
+                !buttonRef.current.contains(event.target)
+            ) {
                 setMenuVisible(false);
             }
         };
 
-        document.addEventListener('mousedown', handleClickOutside);
+        document.addEventListener('click', handleClickOutside);
 
         return () => {
-            document.removeEventListener('mousedown', handleClickOutside);
+            document.removeEventListener('click', handleClickOutside);
         };
-    }, [menuRef]);
+    }, [menuRef, buttonRef]);
+
+    const toggleDropDownMenu = () => {
+        setMenuVisible(!menuVisible);
+    };
 
     return (
         <>
-
-            {menuVisible &&
-
-                <ServiceCardContainer containerRef={menuRef} />
-            }
+            {menuVisible && <ServiceCardContainer containerRef={menuRef} />}
 
             <header className='navbar'>
                 <img src="/vigitech_logo.png" alt='vigitech_logo' className='logo' />
@@ -36,26 +41,36 @@ const Navbar = () => {
                 <nav className='navbar-nav'>
                     <ul className='navbar-ul'>
                         <li>
-                            <NavLink to='/'
-                                className={({ isActive }) => (isActive ? 'navbar-li-selected' : 'navbar-li')}>
+                            <NavLink
+                                to='/'
+                                className={({ isActive }) => (isActive ? 'navbar-li-selected' : 'navbar-li')}
+                            >
                                 Inicio
                             </NavLink>
                         </li>
-                        <li className='navbar-li-menu'
-                            ref={menuRef}
-                            onClick={() => setMenuVisible(prev => !prev)} // Usar la función toggleMenu
-                        >
-                            Servicios
+                        <li>
+                            <button
+                                className='navbar-li-menu'
+                                ref={buttonRef}
+                                onClick={toggleDropDownMenu}
+                            >
+                                Servicios
+                            </button>
+
                         </li>
                         <li>
-                            <NavLink to='/faq'
-                                className={({ isActive }) => (isActive ? 'navbar-li-selected' : 'navbar-li')}>
+                            <NavLink
+                                to='/faq'
+                                className={({ isActive }) => (isActive ? 'navbar-li-selected' : 'navbar-li')}
+                            >
                                 FAQ
                             </NavLink>
                         </li>
                         <li>
-                            <NavLink to='/about'
-                                className={({ isActive }) => (isActive ? 'navbar-li-selected' : 'navbar-li')}>
+                            <NavLink
+                                to='/about'
+                                className={({ isActive }) => (isActive ? 'navbar-li-selected' : 'navbar-li')}
+                            >
                                 Acerca de
                             </NavLink>
                         </li>
@@ -64,14 +79,13 @@ const Navbar = () => {
             </header>
         </>
     );
-}
+};
 
 const ServiceCardContainer = ({ containerRef }) => {
-
     return (
         <div className='modal-overlay'>
             <div className='modal-container' ref={containerRef}>
-                <ServiceCard icon='' title='Vigilancia' options={[{ name: 'Radar', route: '/radar' }]} containerRef={!containerRef} />
+                <ServiceCard icon='' title='Vigilancia' options={[{ name: 'Radar', route: '/radar' }]} />
                 <ServiceCard icon='' title='titulo 2' options={[{ name: 'Servicio 1', route: '#' }, { name: 'Servicio 2', route: '#' }, { name: 'Servicio 3', route: '#' }]} />
                 <ServiceCard icon='' title='titulo 3' options={[{ name: 'Servicio 1', route: '#' }, { name: 'Servicio 2', route: '#' }, { name: 'Servicio 3', route: '#' }]} />
                 <ServiceCard icon='' title='titulo 4' options={[{ name: 'Servicio 1', route: '#' }, { name: 'Servicio 2', route: '#' }, { name: 'Servicio 3', route: '#' }]} />
@@ -82,6 +96,6 @@ const ServiceCardContainer = ({ containerRef }) => {
             </div>
         </div>
     );
-}
+};
 
 export default Navbar;
