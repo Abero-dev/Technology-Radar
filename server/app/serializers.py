e4d0206db3be5525db382f34d73a192f17b903c1
Abero-dev <abel04.mata@gmail.com>
2025-06-05 12:22:47 -0400

    Nest project created

--- server/app/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import Entrie
-
-class EntrieSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Entrie
-        fields = '__all__'
